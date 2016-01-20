49da3ab8c87f7be325be557fd72fd55ce76947d8
epretoriusZA <epretori@za.mhgad.com>
2016-01-20 13:38:52 +0200

    updated test app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
-
+.controller('AboutCtrl', function($scope) {})
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -19,6 +19,11 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+})
+.controller('SideNavCtrl', function($scope, $ionicSideMenuDelegate) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
 })
 
 .controller('AccountCtrl', function($scope) {
